f3028dfe278deed7fbe484a196ad63388a93f7c9
Luka Simpson <31399112+Sublime-css@users.noreply.github.com>
2021-02-07 19:09:21 +1300

    beginning of search function

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 from os import path, system
 import requests
 import json
+from fuzzywuzzy import fuzz
 
 #location of skin JSON file
 json_url = 'https://sublime-css.github.io/ideal-enigma/'
+parse=json.loads(requests.get(json_url).content)
 
 #common game install locations
 directories = [ "C:/Program Files (x86)/Steam/steamapps/common/IL-2 Sturmovik Battle of Stalingrad/", 
@@ -13,24 +15,25 @@ directories = [ "C:/Program Files (x86)/Steam/steamapps/common/IL-2 Sturmovik Ba
                 "E:/SteamLibrary/steamapps/common/IL-2 Sturmovik Battle of Stalingrad/", 
                 #non steam variant... i think?
                 "C:/Program Files/1C Game Studios/IL-2/"]
+                
+def find_install():
+    count = 0
+    for directory in directories:
+        if path.isdir(directory):
+            print("Il-2 directory located at " + directory)
+            count += 1
 
-count = 0
-for directory in directories:
-    if path.isdir(directory):
-        print("Il-2 directory located at " + directory)
-        IL2_path = directory
-        count += 1
+    if count == 0:
+        print(  "Couldn't find your IL-2 installation automatically. Try placing the path, for example " +
+                "C:/Program Files (x86)/Steam/steamapps/common/IL-2 Sturmovik Battle of Stalingrad/, into \"directory.txt\"")
+        system("Pause")
+        exit(1)
 
-if count == 0:
-    print(  "Couldn't find your IL-2 installation automatically. Try placing the path, for example " +
-            "C:/Program Files (x86)/Steam/steamapps/common/IL-2 Sturmovik Battle of Stalingrad/, into \"directory.txt\"")
-    system("Pause")
-    exit(1)
-
-if count > 1:
-    print("Yikes! You appear to have multiple IL-2 installations.")
-    system("Pause")
-    exit(2)
+    if count > 1:
+        print("Yikes! You appear to have multiple IL-2 installations.")
+        system("Pause")
+        exit(2)
+    return directory
 
 #Downloading a skin:
 def download(IL2_path, url, skin_path, name):
@@ -50,17 +53,31 @@ def download(IL2_path, url, skin_path, name):
         return
     print("Successfully synced skin " + name + " to " + skin_path)
 
-print("""ID   Name                                         Airframe                 Creator
+def print_results(search):
+    print("""ID   Name                                         Airframe                 Creator
 ####################################################################################################""")
+    for i in range(1, 1+ len(parse)):
+        if(search == "none" or fuzz.partial_ratio(search.lower(),parse[str(i)]["name"].lower()) > 50):
+            thestring = str(i) + " " * (5 - len(str(i)))
+            thestring += parse[str(i)]["name"] + " " * (45 - len(parse[str(i)]["name"])) 
+            thestring += parse[str(i)]["airframe"] + " " * (25 - len(parse[str(i)]["airframe"]))
+            thestring += parse[str(i)]["creator"] + " " * (25 - len(parse[str(i)]["creator"]))
+            print(thestring)
 
-request = requests.get(json_url)
-parse=json.loads(request.content)
-for i in range(1, 1+ len(parse)):
-    thestring = str(i) + " " * (5 - len(str(i)))
-    thestring += parse[str(i)]["name"] + " " * (45 - len(parse[str(i)]["name"])) 
-    thestring += parse[str(i)]["airframe"] + " " * (25 - len(parse[str(i)]["airframe"]))
-    thestring += parse[str(i)]["creator"] + " " * (25 - len(parse[str(i)]["creator"]))
-    print(thestring)
-
-#download("C:/Users/lukaa/Documents/IL-2/", parse["1"]["host"], parse["1"]["localpath"], parse["1"]["filename"])
-#download("C:/Users/lukaa/Documents/IL-2/", parse["2"]["host"], parse["2"]["localpath"], parse["2"]["filename"])
+while True:
+    print_results("none")
+    bob = input("Skin to sync, for example '1'. '0' or 'exit' to exit\n")
+    if (bob == "0" or bob.lower() == "exit"):
+        exit()
+    elif (bob.lower() == "search"):
+        print("Search Mode")
+        while True:
+            bob = input("Search term, for example 'FW190'. '0' or 'exit' to exit search mode and return to normal operation\n")
+            if (bob == "0" or bob.lower() == "exit"):
+                break
+            print_results(bob)
+    else:    
+        try:
+            download("C:/Users/lukaa/Documents/IL-2/", parse[bob]["host"], parse[bob]["localpath"], parse[bob]["filename"])
+        except:
+            print("Error! input out of bounds")
